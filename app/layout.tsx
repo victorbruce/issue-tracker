c42c9ac02b5901d5439b66a08b274fa5ec895e7f
victorbruce <victorbruce82@gmail.com>
2023-11-09 08:51:37 +0000

    setup radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import NavBar from './NavBar'
+import "@radix-ui/themes/styles.css";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import NavBar from "./NavBar";
+import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Issue Tracker',
-  description: 'An issue tracker app',
-}
+  title: "Issue Tracker",
+  description: "An issue tracker app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
-  )
+  );
 }
