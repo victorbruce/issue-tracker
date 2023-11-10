7ad81398206ad63f4ca393f4819b7911f015b5cd
victorbruce <victorbruce82@gmail.com>
2023-11-10 07:00:19 +0000

    show the issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -28,14 +28,14 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
-  const onSubmit = async (data: IssueForm) => {
+  const onSubmit = handleSubmit(async (data: IssueForm) => {
     try {
       await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setError("An unexpected error occured.");
     }
-  };
+  });
 
   return (
     <div className="max-w-xl">
@@ -44,7 +44,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
