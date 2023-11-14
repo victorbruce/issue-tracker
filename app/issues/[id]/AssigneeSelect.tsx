f64fa82a0ddbd6037f4d52df629608a5b1d2c38f
victorbruce <victorbruce82@gmail.com>
2023-11-14 06:44:02 +0000

    filter issues

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -24,7 +24,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   return (
     <>
       <Select.Root
-        defaultValue={issue.assignedToUserId || ""}
+        defaultValue={issue.assignedToUserId || "unassigned"}
         onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Unassigned" />
---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const statuses: { label: string; value?: Status }[] = [
+  {
+    label: "All",
+  },
+  {
+    label: "Open",
+    value: "OPEN",
+  },
+  {
+    label: "In Progress",
+    value: "IN_PROGRESS",
+  },
+  {
+    label: "Closed",
+    value: "CLOSED",
+  },
+];
+
+const IssueStatusFilter = () => {
+  const router = useRouter();
+  return (
+    <Select.Root
+      defaultValue="ALL"
+      onValueChange={(status) => {
+        const query = status === "ALL" ? "" : `?status=${status}`;
+        router.push("/issues/list" + query);
+      }}
+    >
+      <Select.Trigger />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.value} value={status.value || "ALL"}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
